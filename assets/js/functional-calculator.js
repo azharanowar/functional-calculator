1da7e3c8cc20f60c2a4ad0040dd4854387ce11fc
azharanowar <azharanowar@gmail.com>
2022-08-30 20:32:06 +0600

    Functional calculator completed

---
+++ assets/js/functional-calculator.js
@@ -0,0 +1,64 @@
+document.getElementById("calculatorKeysSection").addEventListener('click', function( event ) {
+    if ( event.target.classList.contains("calculator-btn") ) {
+        const targetedEventInnerText = event.target.innerText;
+        const targetedEventValue = targetedEventInnerText.toLowerCase();
+        
+        switch( targetedEventValue ) {
+            case "ac":
+                clearDisplay();
+                break;
+            case "x":
+                backspace();
+                break;
+            case "=":
+                resultCalculation();
+                break;
+            default:
+                expression( targetedEventValue );
+                const fullExpressionSecondLastIndex = parseFloat( fullExpression[fullExpression.length - 2] );
+                const fullExpressionLastIndex = parseFloat( fullExpression[fullExpression.length - 1] );
+                console.log( fullExpressionSecondLastIndex, fullExpressionLastIndex )
+
+                if ( isNaN( fullExpressionSecondLastIndex ) && isNaN(fullExpressionLastIndex) ) {
+                    fullExpression = fullExpression.slice(0, fullExpression.length - 2) + targetedEventValue;
+                    expression('')
+                }
+
+                // for ( i = 0; i < fullExpression.length; i++ ) {
+                //     console.log(fullExpression[i], fullExpression[i+1]);
+                //     console.log(isNaN(parseFloat(fullExpression[i])), isNaN(parseFloat(fullExpression[i+1])));
+                //     if ( isNaN(parseFloat(fullExpression[i])) && isNaN(parseFloat(fullExpression[i+1])) ) {
+                //         console.log("Hello")
+                //     }
+                // }
+                break;
+                
+        }
+    }
+});
+
+let fullExpression = ''; 
+
+function expression( newExpression ) {
+    fullExpression += newExpression;
+    document.getElementById("expressionDisplay").innerText = fullExpression;
+}
+
+
+function backspace() {
+    fullExpression = fullExpression.slice(0, fullExpression.length - 1);
+    expression('')
+}
+
+function resultCalculation() {
+    const result = eval(fullExpression);
+    document.getElementById("resultDisplay").innerText = result;
+    fullExpression = result;
+    expression('')
+}
+
+function clearDisplay() {
+    document.getElementById("resultDisplay").innerText = "00";
+    document.getElementById("expressionDisplay").innerText = "";
+    fullExpression = '';
+}
